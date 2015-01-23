e64f7cc21c192d9ad7e26eeb13d063b872b6ac2a
alumno <vzalberto>
2015-01-23 13:22:41 -0600

    Programa 7-2

--- main.c
+++ main.c
@@ -1,19 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-	int i = 10, pid;
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/sem.h>
+
+#define SEMAFORO_PADRE 1
+#define SEMAFORO_HIJO 0
+
+int main(int argc, char *argv[]){
+	int semid,i = 10, pid;
+	struct sembuf operacion;
+	key_t llave;
+
+	llave = ftok(argv[0], 'U');
+
+	if((semid = semget(llave, 2, IPC_CREAT | 0600)) == -1){
+		perror("Error en semget");
+		exit(-1);
+	}
+	semctl(semid, SEMAFORO_HIJO, SETVAL, 0);
+	semctl(semid, SEMAFORO_PADRE, SETVAL, 1);
 
 	if((pid = fork()) == -1){
 		perror("fork");
 		exit(-1);
 	}
 	else if (pid == 0){
-		while (i)
-			printf("PROCESO HIJO: %d\n", i--);
+		while (i){
+			operacion.sem_flg = 0;
+			operacion.sem_op = -1;
+			operacion.sem_num = SEMAFORO_HIJO;
+			semop(semid, &operacion, 1);
+
+			printf("SOY EL PROCESO HIJO: %d\n", i--);
+
+			operacion.sem_op = 1;
+			operacion.sem_num = SEMAFORO_PADRE;
+			semop(semid, &operacion, 1);
+		}
+
+		semctl(semid, 0, IPC_RMID, 0);
 	}else{
-			while(i)
-				printf("PROCESO PADRE: %d\n", i--);
+			while(i){
+			operacion.sem_flg = 0;
+			operacion.sem_op = -1;
+			operacion.sem_num = SEMAFORO_PADRE;
+
+			semop(semid, &operacion, 1);
+			printf("SOY EL PROCESO PADRE: %d\n", i--);
+
+			operacion.sem_op = 1;
+			operacion.sem_num = SEMAFORO_HIJO;
+			semop(semid, &operacion, 1);
+
+			}
+
+			semctl(semid, 0, IPC_RMID, 0);
 		}
 
 	return 0;
